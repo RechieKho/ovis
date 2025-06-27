740a7d16579356b52f16e59a0f4f8222985d2a23
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2025-06-27 15:32:09 +0800

    Add int64 builder node.

---
+++ include/ovis/ast/nodes/int64_builder_node.hpp
@@ -0,0 +1,43 @@
+#ifndef OVIS_AST_NODES_INT64_BUILDER_NODE_HPP
+#define OVIS_AST_NODES_INT64_BUILDER_NODE_HPP
+
+#include <ovis/ast/nodes/builder_node.hpp>
+
+namespace ovis::ast
+{
+
+    namespace implementation
+    {
+
+        template <c_is_generator t_generator_type>
+        class int64_builder_node : public expression_builder_node<t_generator_type>
+        {
+        public:
+            using generator_type = t_generator_type;
+            using base_type = expression_builder_node<generator_type>;
+            using result_type = typename base_type::result_type;
+            using expression_builder_node_box_type = typename base_type::expression_builder_node_box_type;
+            using optional_token_type = typename base_type::optional_token_type;
+
+        private:
+            int64_type m_value;
+
+        public:
+            explicit int64_builder_node(int32_type p_value, optional_token_type p_token = optional_token_type())
+                : base_type(std::move(p_token)), m_value(p_value) {}
+
+            ~int64_builder_node() override = default;
+
+            auto generate() const -> result_type override
+            {
+                return generator_type::get_singleton().generate_int64(m_value);
+            }
+
+            auto get() const -> int64_type { return m_value; }
+        };
+
+    } // namespace implementation
+
+} // namespace ovis::ast
+
+#endif // OVIS_AST_NODES_INT64_BUILDER_NODE_HPP
