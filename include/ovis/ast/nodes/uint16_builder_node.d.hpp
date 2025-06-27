de11fe25ab7582c307d652383809720cc2ed8642
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2025-06-27 15:39:40 +0800

    add unsigned counterpart.

---
+++ include/ovis/ast/nodes/uint16_builder_node.d.hpp
@@ -0,0 +1,19 @@
+#ifndef OVIS_AST_NODES_UINT16_BUILDER_NODE_H_HPP
+#define OVIS_AST_NODES_UINT16_BUILDER_NODE_H_HPP
+
+#include <ovis/ast/nodes/builder_node.hpp>
+
+namespace ovis::ast
+{
+
+    namespace implementation
+    {
+
+        template <c_is_generator t_generator_type>
+        class uint16_builder_node;
+
+    } // namespace implementation
+
+} // namespace ovis::ast
+
+#endif // OVIS_AST_NODES_UINT16_BUILDER_NODE_H_HPP
---
+++ include/ovis/ast/nodes/uint64_builder_node.d.hpp
@@ -0,0 +1,19 @@
+#ifndef OVIS_AST_NODES_UINT64_BUILDER_NODE_H_HPP
+#define OVIS_AST_NODES_UINT64_BUILDER_NODE_H_HPP
+
+#include <ovis/ast/nodes/builder_node.hpp>
+
+namespace ovis::ast
+{
+
+    namespace implementation
+    {
+
+        template <c_is_generator t_generator_type>
+        class uint64_builder_node;
+
+    } // namespace implementation
+
+} // namespace ovis::ast
+
+#endif // OVIS_AST_NODES_UINT64_BUILDER_NODE_H_HPP
